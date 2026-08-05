9f7bb6f1eb914b6d7a6f141a2d9b85056cc41649
Jankar-shivani08 <jankarshivani24@gmail.com>
2026-08-05 10:12:11 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ class Hello {
         System.out.println("Hello, World!");
         System.out.println("Jenkins");
         System.out.println("Running the java program in Jenkin will code changing");
+        System.out.println("we make the changes");
     }
 }
